550f1715269bda27fbed0ce7a86b125892749967
bartomsilva <bartomsilva@gmail.com>
2023-02-04 11:56:26 -0300

    state

--- React/states/template/src/Componentes/Carro/styles.jsx
+++ React/states/template/src/Componentes/Carro/styles.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
 export const CarroContainer = styled.section`
-  width: 200px;
+  width: 350px;
   border: 1px solid black;
-  display: flex;
+  /* display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: center; */
   gap: 1rem;
   padding: 1rem;
   
