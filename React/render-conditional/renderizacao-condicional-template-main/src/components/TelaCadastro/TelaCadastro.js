e31c311bf1ad399bde8386dc5eb269b9f38669c3
bartomsilva <bartomsilva@gmail.com>
2023-02-24 15:41:51 -0300

    exercicios

---
+++ React/render-conditional/renderizacao-condicional-template-main/src/components/TelaCadastro/TelaCadastro.js
@@ -0,0 +1,32 @@
+import { Form, FormContainer, Input, StyledLabel, SendButton, BackToLoginButton } from "./styled";
+
+function TelaCadastro(props) {
+  const {setPageActive} = props
+  return (
+    <FormContainer>
+      <h1>Cadastro </h1>
+      <Form>
+        <StyledLabel htmlFor="titulo">
+          Nome:
+          <Input id="titulo" />
+        </StyledLabel>
+        <StyledLabel htmlFor="foto">
+          E-mail:
+          <Input id="foto" />
+        </StyledLabel>
+        <StyledLabel htmlFor="descricao">
+          Senha:
+          <Input id="descricao" />
+        </StyledLabel>
+        <StyledLabel htmlFor="descricao">
+          Confirmação da senha:
+          <Input id="descricao" />
+        </StyledLabel>
+        <SendButton onClick={()=>setPageActive("registered")} >Cadastrar</SendButton>
+        <BackToLoginButton onClick={()=>setPageActive('login')} >Já possuo um cadastro</BackToLoginButton>
+      </Form>
+    </FormContainer>
+  );
+}
+
+export default TelaCadastro;
